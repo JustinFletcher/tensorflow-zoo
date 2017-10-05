a6e28fa0abf20e7cbce7debdffb32c9ebd29dcb4
Justin Fletcher <justinr.fletcher@gmail.com>
2017-10-04 22:04:14 -1000

    Modified converter to output to data dir.

--- convert_to_records.py
+++ convert_to_records.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--directory',
         type=str,
-        default='.',
+        default='../data',
         help='Directory to download data files and write the converted result'
     )
     parser.add_argument(
