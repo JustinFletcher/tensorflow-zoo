a92b5546bb0abf3a5c7c2991dedfd9b2561c37eb
Justin Fletcher <justinr.fletcher@gmail.com>
2017-10-05 20:45:20 -1000

    Doubled queue threads.

--- model_class_supervisor_queue.py
+++ model_class_supervisor_queue.py
@@ -127,7 +127,7 @@ def inputs(train, batch_size, num_epochs):
         images, sparse_labels = tf.train.shuffle_batch(
             [image, label],
             batch_size=batch_size,
-            num_threads=8,
+            num_threads=16,
             capacity=1000000,
             # Ensures a minimum amount of shuffling of examples.
             min_after_dequeue=1000)
