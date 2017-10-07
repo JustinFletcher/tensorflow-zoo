75503ca322910043d8e713cfbb2b7dbf72ac9b62
Justin Fletcher <justinr.fletcher@gmail.com>
2017-10-06 16:01:18 -1000

    Parameter.

--- model_class_supervisor_queue.py
+++ model_class_supervisor_queue.py
@@ -127,8 +127,8 @@ def inputs(train, batch_size, num_epochs):
         images, sparse_labels = tf.train.shuffle_batch(
             [image, label],
             batch_size=batch_size,
-            capacity=10.0 * batch_size,
-            num_threads=1,
+            capacity=10000000000000000000.0 * batch_size,
+            num_threads=2,
             min_after_dequeue=1)
 
     return images, sparse_labels
