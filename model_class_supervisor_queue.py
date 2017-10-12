d179eb2a5a43db3552c7ac5adc6436856c9101cf
Justin Fletcher <justinr.fletcher@gmail.com>
2017-10-12 12:03:41 -1000

    Increased thread count.

--- model_class_supervisor_queue.py
+++ model_class_supervisor_queue.py
@@ -128,7 +128,7 @@ def inputs(train, batch_size, num_epochs):
             [image, label],
             batch_size=batch_size,
             capacity=1000000.0 * batch_size,
-            num_threads=15,
+            num_threads=50,
             min_after_dequeue=1)
 
     return images, sparse_labels
