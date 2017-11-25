cd4ab9e56b93addbfaacf82430ea9505dd859344
Justin Fletcher <justinr.fletcher@gmail.com>
2017-11-24 16:03:08 -1000

    Buildup.

--- sample_dist_experiment.py
+++ sample_dist_experiment.py
@@ -1,3 +1,10 @@
 import os
 
+
+print("-------")
+print(os.environ['PBS_TASKNUM'])
 print(os.environ['PBS_NODENUM'])
+print(os.environ['PBS_VNODENUM'])
+print("-------")
+
+print(os.environ['$PBS_NODENUM'])
