23c732ec29b2e0632fc93bc2172430c0fa866bb3
Justin Fletcher <justinr.fletcher@gmail.com>
2017-11-24 16:05:39 -1000

    Debug.

--- sample_dist_experiment.py
+++ sample_dist_experiment.py
@@ -4,7 +4,5 @@ import os
 print("-------")
 print(os.environ['PBS_TASKNUM'])
 print(os.environ['PBS_NODENUM'])
-print(os.environ['PBS_VNODENUM'])
-print("-------")
-
 print(os.environ['$PBS_NODENUM'])
+print("-------")
